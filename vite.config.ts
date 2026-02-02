dd7357e96d3fa939a66c02bb08994053a134c0c6
Eric Zhang <ekzhang1@gmail.com>
2026-02-01 17:15:34 -0800

    Fix plugin getting packaged incorrectly

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
-import ghPagesSpa from "@sctg/vite-plugin-github-pages-spa";
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.BASE_PATH || "/",
-  plugins: [svelte(), tailwindcss(), ghPagesSpa()],
+  plugins: [svelte(), tailwindcss(), githubPagesSpa()],
 });
